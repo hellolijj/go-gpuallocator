ec5e2cbdb2b56e01f10cb73562d101b56a55f81c
Junjun Li <junjunli666@gmail.com>
2019-07-14 10:54:31 +0800

    add main to test

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/NVIDIA/go-gpuallocator/gpuallocator"
+)
+
+func main() {
+	allocator, err := gpuallocator.NewSimpleAllocator()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+	}
+
+	for _, gpu := range allocator.GPUs {
+		fmt.Printf("%v\n", gpu.Details())
+	}
+
+	fmt.Printf("\n")
+
+	for _, i := range []int{1, 2, 4, 8} {
+		gpus := allocator.Allocate(i)
+		fmt.Printf("Simple allocation of %v GPUs: %v\n", i, gpus)
+		allocator.Free(gpus...)
+	}
+}
